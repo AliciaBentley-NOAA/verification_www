fa7864738e65d1b09c8f9df4d2fa4ca7ba916724
alicia bentley <alicia.bentley@clogin05.cactus.wcoss2.ncep.noaa.gov>
2022-09-14 17:25:43 +0000

    Added /global/naefs/ops contents

---
+++ global/naefs/ops/loops/naefs_info.php
@@ -0,0 +1,38 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en" lang="en">
+
+<head>
+<meta http-equiv="content-type" content="text/html; charset=utf-8" />
+<title>Home</title>
+<link href="http://fonts.googleapis.com/css?family=Source+Sans+Pro:200,300,400,600,700,900" rel="stylesheet" />
+<link href="main.css" rel="stylesheet" type="text/css" media="all" />
+<link href="fonts.css" rel="stylesheet" type="text/css" media="all" />
+<script src="https://d3js.org/d3.v4.min.js"></script>
+</head>
+
+
+<body>
+<u>All Images:</u>
+<br>
+"E20s" (<font style="font-weight: bold" color="black">black line</font>) represents the 20 ensemble members in NCEP's Global Ensemble Forecast System (GEFS). 
+<br>
+"E20m" (<font style="font-weight: bold" color="red">red line</font>) represents the 20 ensemble members in CMC's Global Ensemble Prediction System (GEPS).
+<br>
+"E40n" (<font style="font-weight: bold" color="limegreen">green line</font>) represents the 40 ensemble members in the North American Ensemble Forecast System (NAEFS).
+<br>
+<br>
+<u>RMSE/Spread Images:</u>
+<br>
+<b>Solid lines</b> represent ensemble mean RMSE. <b>Dashed lines</b> represent ensemble spread.
+<br>
+<br>
+<u>Brier Skill Score Reliability/Resolution Images:</u>
+<br>
+<b>Solid lines</b> represent reliability. <b>Dashed lines</b> represent resolution.
+<br>
+<br>
+<u>Ensemble Mean Error and Absolute Error Images:</u>
+<br>
+<b>Solid lines</b> represent ensemble mean error. <b>Dashed lines</b> represent ensemble absolute error.
+</body>
+</html>
